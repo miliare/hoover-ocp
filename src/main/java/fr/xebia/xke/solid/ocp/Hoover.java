ec2f720e6d52d93df7952fbcc3cead0da2dedc82
Rémy Milia <milia.remy@gmail.com>
2018-08-06 18:33:02 +0200

    apply strategy pattern for ocp

--- src/main/java/fr/xebia/xke/solid/ocp/Hoover.java
+++ src/main/java/fr/xebia/xke/solid/ocp/Hoover.java
@@ -1,96 +1,54 @@
 package fr.xebia.xke.solid.ocp;
 
-import static fr.xebia.xke.solid.ocp.Orientation.EAST;
-import static fr.xebia.xke.solid.ocp.Orientation.NORTH;
-import static fr.xebia.xke.solid.ocp.Orientation.SOUTH;
-import static fr.xebia.xke.solid.ocp.Orientation.WEST;
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
 
 public class Hoover {
 
+    public static final int TURN_RIGHT = 90;
+    public static final int TURN_LEFT = -90;
     private final String program;
     private Position currentPosition = new Position();
-    private Orientation currentOrientation = Orientation.NORTH;
 
     public Hoover(String program) {
         this.program = program;
     }
 
     public void clean() {
-        for (int i = 0 ; i < program.length() ; i++) {
-            switch (program.charAt(i)) {
-                case('A'):
-                    goForward();
-                    break;
-                case('D'):
-                    turnRight();
-                    break;
-                case('G'):
-                    turnLeft();
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid command: " + program.charAt(i));
-            }
+        for (int i = 0; i < program.length(); i++) {
+            currentPosition = Strategy.of(program.charAt(i)).compute(currentPosition);
         }
     }
 
-    private void turnLeft() {
-        switch(currentOrientation) {
-            case NORTH:
-                currentOrientation = WEST;
-                break;
-            case WEST:
-                currentOrientation = SOUTH;
-                break;
-            case SOUTH:
-                currentOrientation = EAST;
-                break;
-            case EAST:
-                currentOrientation = NORTH;
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid orientation: " + currentOrientation.name());
-        }
-    }
-
-    private void turnRight() {
-        switch(currentOrientation) {
-            case NORTH:
-                currentOrientation = EAST;
-                break;
-            case EAST:
-                currentOrientation = SOUTH;
-                break;
-            case SOUTH:
-                currentOrientation = WEST;
-                break;
-            case WEST:
-                currentOrientation = NORTH;
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid orientation: " + currentOrientation.name());
-        }
-    }
-
-    private void goForward() {
-        switch(currentOrientation) {
-            case NORTH:
-                currentPosition = new Position(currentPosition.x, currentPosition.y + 1);
-                break;
-            case EAST:
-                currentPosition = new Position(currentPosition.x + 1, currentPosition.y);
-                break;
-            case SOUTH:
-                currentPosition = new Position(currentPosition.x, currentPosition.y - 1);
-                break;
-            case WEST:
-                currentPosition = new Position(currentPosition.x - 1, currentPosition.y);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid orientation: " + currentOrientation.name());
-        }
-    }
 
     public Position getPosition() {
         return currentPosition;
+    }
+
+    private enum Strategy {
+
+        FORWARD('A', (currentPosition) -> new Position(currentPosition.x + currentPosition.orientation.plusX, currentPosition.y + currentPosition.orientation.plusY, currentPosition.orientation)),
+        RIGHT('D', (currentPosition) -> new Position(currentPosition.x, currentPosition.y, currentPosition.orientation.turn(TURN_RIGHT))),
+        LEFT('G', (currentPosition) -> new Position(currentPosition.x, currentPosition.y, currentPosition.orientation.turn(TURN_LEFT)));
+
+        private final char direction;
+        private final UnaryOperator<Position> move;
+
+        Strategy(char direction, UnaryOperator<Position> move) {
+            this.direction = direction;
+            this.move = move;
+        }
+
+        public static Strategy of(char direction) {
+            return Arrays
+                    .stream(values())
+                    .filter(strategy -> strategy.direction == direction)
+                    .findAny()
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid command: " + direction));
+        }
+
+        public Position compute(Position currentPosition) {
+            return move.apply(currentPosition);
+        }
     }
 }
--- src/main/java/fr/xebia/xke/solid/ocp/Orientation.java
+++ src/main/java/fr/xebia/xke/solid/ocp/Orientation.java
@@ -1,5 +1,36 @@
 package fr.xebia.xke.solid.ocp;
 
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
 public enum Orientation {
-    NORTH,EAST,SOUTH,WEST
+    NORTH(0, 0, 1),
+    EAST(90, 1, 0),
+    SOUTH(180, 0, -1),
+    WEST(270, -1, 0);
+
+    public int angle;
+    public int plusX;
+    public int plusY;
+
+    Orientation(int angle, int plusX, int plusY) {
+        this.angle = angle;
+        this.plusX = plusX;
+        this.plusY = plusY;
+    }
+
+    private static Orientation fromAngle(int newAngle) {
+        return Arrays
+                .stream(values())
+                .filter(orientation -> orientation.angle == newAngle)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid angle: " + newAngle));
+    }
+
+    public Orientation turn(int angle) {
+        int newAngle = abs(this.angle + angle) % 360;
+        return Orientation.fromAngle(newAngle);
+    }
+
 }
